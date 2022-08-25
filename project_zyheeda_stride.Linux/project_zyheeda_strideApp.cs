c80a85aa45325b6e717ee3d731fc83ea63a57a04
codaishin <codaishin@gmail.com>
2022-08-25 20:48:10 +0200

    fix(styling): namespace and auto formatting

--- project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
+++ project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
@@ -1,9 +1,3 @@
-namespace ProjectZyheeda;
 using Stride.Engine;
-
-class project_zyheeda_strideApp {
-	static void Main(string[] args) {
-		using var game = new Game();
-		game.Run();
-	}
-}
+using var game = new Game();
+game.Run();
