d5dfebac8d14c56a31b4213683fc6c56ecd4bb21
codaishin <codaishin@gmail.com>
2023-01-08 16:06:42 +0100

    feat(service): add SystemMessage

--- project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
+++ project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
@@ -4,4 +4,5 @@ using Stride.Engine;
 using var game = new Game();
 game.Services.AddService<IInputManagerWrapper>(new InputManagerWrapper(game));
 game.Services.AddService<IGetAnimation>(new GetAnimations());
+game.Services.AddService<ISystemMessage>(new SystemMessage());
 game.Run();
