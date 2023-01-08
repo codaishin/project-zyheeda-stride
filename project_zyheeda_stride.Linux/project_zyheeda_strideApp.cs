ca98c485c9ccd0a33464651cef98a042cd177b90
codaishin <codaishin@gmail.com>
2023-01-08 16:32:48 +0100

    feat(services): add PlayerMessage

--- project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
+++ project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
@@ -5,4 +5,5 @@ using var game = new Game();
 game.Services.AddService<IInputManagerWrapper>(new InputManagerWrapper(game));
 game.Services.AddService<IGetAnimation>(new GetAnimations());
 game.Services.AddService<ISystemMessage>(new SystemMessage());
+game.Services.AddService<IPlayerMessage>(new PlayerMessage(game));
 game.Run();
