f9f8a56a86e4849005085438d881059511937e75
codaishin <codaishin@gmail.com>
2023-04-28 20:24:37 +0200

    revert: services not added to game manually

--- project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
+++ project_zyheeda_stride.Linux/project_zyheeda_strideApp.cs
@@ -1,10 +1,4 @@
-using ProjectZyheeda;
 using Stride.Engine;
 
 using var game = new Game();
-game.Services.AddService<IInputManagerWrapper>(new InputManagerWrapper(game));
-game.Services.AddService<IAnimation>(new Animation());
-game.Services.AddService<ISystemMessage>(new SystemMessage());
-game.Services.AddService<IPlayerMessage>(new PlayerMessage(game));
-game.Services.AddService<IPrefabLoader>(new PrefabLoader());
 game.Run();
