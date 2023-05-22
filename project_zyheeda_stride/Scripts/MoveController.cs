a590d00c7ead3576c828f41d8c7a7205f112f044
codaishin <codaishin@gmail.com>
2023-05-22 18:29:21 +0200

    feat(scripts): moveController non-generic

--- project_zyheeda_stride/Scripts/MoveController.cs
+++ project_zyheeda_stride/Scripts/MoveController.cs
@@ -5,14 +5,8 @@ using System.Linq;
 using Stride.Engine;
 
 
-public abstract class BaseMoveController<TAnimatedMove> :
-	ProjectZyheedaStartupScript,
-	IEquipment
-	where TAnimatedMove :
-		IAnimatedMove,
-		new() {
-
-	public readonly TAnimatedMove move = new();
+public class MoveController : ProjectZyheedaStartupScript, IEquipment {
+	public IAnimatedMove? move;
 
 	private static Either<U<SystemStr, PlayerStr>, AnimationComponent> AnimatorOnChildOf(Entity agent) {
 		return agent
@@ -22,17 +16,25 @@ public abstract class BaseMoveController<TAnimatedMove> :
 			.ToEither(new U<SystemStr, PlayerStr>(new SystemStr($"Missing AnimationComponent on {agent.Name}")));
 	}
 
+	private Either<U<SystemStr, PlayerStr>, IAnimatedMove> EitherMoveOrError() {
+		return this.move is null
+			? new Either<U<SystemStr, PlayerStr>, IAnimatedMove>(new SystemStr(this.MissingField(nameof(this.move))))
+			: new Either<U<SystemStr, PlayerStr>, IAnimatedMove>(this.move);
+	}
+
 	public Either<IEnumerable<U<SystemStr, PlayerStr>>, FGetCoroutine> PrepareCoroutineFor(Entity agent) {
-		var prepareCoroutine = (AnimationComponent agentAnimator) =>
-			this.move.PrepareCoroutineFor(
+		var prepareCoroutine = (IAnimatedMove move) => (AnimationComponent agentAnimator) =>
+			move.PrepareCoroutineFor(
 				agent,
 				speedPerSecond => (float)this.Game.UpdateTime.Elapsed.TotalSeconds * speedPerSecond,
 				key => this.Play(key, agentAnimator)
 			);
 
 
-		var agentAnimator = BaseMoveController<AnimatedStraightMove>.AnimatorOnChildOf(agent);
-		return prepareCoroutine.ApplyWeak(agentAnimator);
+		var agentAnimatorOrError = MoveController.AnimatorOnChildOf(agent);
+		return prepareCoroutine
+			.ApplyWeak(this.EitherMoveOrError())
+			.ApplyWeak(agentAnimatorOrError);
 	}
 
 	private void Play(string animationKey, AnimationComponent agentAnimator) {
@@ -42,5 +44,3 @@ public abstract class BaseMoveController<TAnimatedMove> :
 		}
 	}
 }
-
-public class MoveController : BaseMoveController<AnimatedStraightMove> { }
