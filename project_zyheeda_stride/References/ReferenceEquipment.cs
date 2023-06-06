1f44a9541e2bbbd09f8d0f8e2d0ec4533022666e
codaishin <codaishin@gmail.com>
2023-06-06 19:10:56 +0200

    feat: new reference for IEquipment in the Editor

--- project_zyheeda_stride/References/ReferenceEquipment.cs
+++ project_zyheeda_stride/References/ReferenceEquipment.cs
@@ -1,5 +1,19 @@
 namespace ProjectZyheeda;
 
-using Stride.Core;
+using Stride.Engine;
 
-[DataContract] public class ReferenceMoveController : Reference<MoveController, IEquipment> { }
+public abstract class ReferenceEquipment<TEquipment> : Reference<TEquipment>, IEquipmentEditor
+	where TEquipment :
+		IEquipment {
+
+	public TEquipment? Target {
+		get => this.GetRef();
+		set => this.SetRef(value);
+	}
+
+	public Result<FGetCoroutine> PrepareCoroutineFor(Entity agent) {
+		return this.target.FlatMap(e => e.PrepareCoroutineFor(agent));
+	}
+}
+
+public class ReferenceMoveController : ReferenceEquipment<MoveController> { }
