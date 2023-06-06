7dd0be2087a13a035f3e029255556e47f34b62a5
codaishin <codaishin@gmail.com>
2023-06-06 19:09:44 +0200

    feat(tools): Reference for editor interfaces

--- Tests/References/Reference.test.cs
+++ Tests/References/Reference.test.cs
@@ -1,10 +1,11 @@
 namespace Tests;
 
+using System.Linq;
 using ProjectZyheeda;
 using Stride.Engine;
 using Xunit;
 
-public class ReferenceTest {
+public class ReferenceTests {
 	public interface IMock { }
 
 	private class MockComponent : StartupScript, IMock { }
@@ -25,5 +26,49 @@ public class ReferenceTest {
 			target = new(),
 		};
 		Assert.Same(reference.target, reference.UnpackOr(fallback));
+	}
+
+	private class StringReference : Reference<string> {
+		public string? Target {
+			get => this.GetRef();
+			set => this.SetRef(value);
+		}
+
+		public Result<string> ExposedInnerReference => this.target;
+	}
+
+	[Fact]
+	public void Ok() {
+		var reference = new StringReference { Target = "42" };
+
+		Assert.Equal("42", reference.ExposedInnerReference.UnpackOr("-1"));
+	}
+
+	[Fact]
+	public void NotSet() {
+		var reference = new StringReference();
+		var errors = reference.ExposedInnerReference.Switch(
+			errors => (
+				string.Join(", ", errors.system.Select(e => (string)e)),
+				string.Join(", ", errors.player.Select(e => (string)e))
+			).ToString(),
+			_ => "no errors"
+		);
+
+		Assert.Equal((reference.MissingField("target"), "").ToString(), errors);
+	}
+
+	[Fact]
+	public void SetNull() {
+		var reference = new StringReference { Target = null };
+		var errors = reference.ExposedInnerReference.Switch(
+			errors => (
+				string.Join(", ", errors.system.Select(e => (string)e)),
+				string.Join(", ", errors.player.Select(e => (string)e))
+			).ToString(),
+			_ => "no errors"
+		);
+
+		Assert.Equal((reference.MissingField("target"), "").ToString(), errors);
 	}
 }
--- project_zyheeda_stride/References/Reference.cs
+++ project_zyheeda_stride/References/Reference.cs
@@ -14,3 +14,39 @@ public abstract class Reference<TTarget, TInterface> :
 		return this.target is not null ? some(this.target) : none();
 	}
 }
+
+/// <summary>
+/// 	Base class for all interface references that need to be assignable as components in the
+/// 	editor.
+///
+/// 	Stride seemingly can't handle (de)serialization of generic fields/properties that remain
+/// 	generic for more than one level of inheritance. Thus, we cannot declare the Target property
+/// 	(the component we are referencing) here. The base class for each type of interface must do
+/// 	that instead.
+///
+/// 	However, we expose some quasi setter/getter methods that can be used for the get and set on
+/// 	the children's Target property.
+/// </summary>
+/// <typeparam name="TTarget"></typeparam>
+[DataContract(Inherited = true)]
+public abstract class Reference<TTarget> {
+	protected Result<TTarget> target;
+
+	public Reference() {
+		this.target = Result.SystemError(this.MissingField(nameof(this.target)));
+	}
+
+	protected TTarget? GetRef() {
+		return this.target.Switch<TTarget?>(
+			_ => default,
+			v => v
+		);
+	}
+
+	protected void SetRef(TTarget? value) {
+		this.target =
+			value is null
+				? Result.SystemError(this.MissingField(nameof(this.target)))
+				: Result.Ok(value);
+	}
+}
